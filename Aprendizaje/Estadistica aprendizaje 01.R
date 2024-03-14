e3338b78e8a6d93a60edbae8c320fc479a014769
cyreyes <70662883+cyreyes0@users.noreply.github.com>
2024-03-13 19:19:42 -0500

    Create Estadistica aprendizaje 01.R

---
+++ Aprendizaje/Estadistica aprendizaje 01.R
@@ -0,0 +1,42 @@
+install.packages("tidyverse", dep = TRUE)
+
+remove.packages(c("ggplot2", "data.table"))
+install.packages('ggplot2', dep = TRUE)
+
+library(tidyverse)
+
+install.packages("magic", dep = TRUE)
+
+library(magic)
+
+magic(6)
+magic(4)
+
+Inf
+
+pi
+
+-Inf
+
+NaN
+
+NA
+
+choose(5,10)
+factorial(5)
+choose(10,5)
+choose(3,2)
+
+#Funciones
+unaVariable = 12
+doble = function(x){2*x}
+doble(unaVariable)
+
+cuadrado = function(x){x^2}
+cuadrado(unaVariable)
+
+
+
+
+
+
