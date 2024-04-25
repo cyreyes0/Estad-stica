acadd61be35e21194e54950153dbd81b71fd311f
cyreyes <70662883+cyreyes0@users.noreply.github.com>
2024-04-25 13:38:09 -0300

    Pyhton Folder

--- Aprendizaje/Tarea 1 - 03.R
+++ Aprendizaje/Tarea 1 - 03.R
@@ -13,3 +13,35 @@ rest_a = total - dias
 rest_a
 #6 de dic, a las 12 y algo del dia, en el año 2025
 
+#Crea una función en R que resuelva una ecuación de primer grado 
+#(de la forma Ax + B = 0). Es decir, los
+#parámetros deben ser los coeficientes (en orden) y la función tiene
+#que devolver la solución. 
+
+ecu_pri = function(a,b,c){
+  x = (c-b)/a
+  x
+}
+
+print(ecu_pri(2,4,0))
+print(ecu_pri(5,3,0))
+print(ecu_pri(7,4,18))
+print(ecu_pri(1,1,1))
+
+#Da una expresión para calcular 3e−π con R y a continuación, da el resultado 
+#obtenido redondeado a 3 cifras decimales.
+
+e = exp(1)
+expre = 3*e-pi
+print(expre, 3)
+
+#Pregunta 4
+ima = (2+3i)^2/(5+8i)
+print(ima, 3)
+
+
+
+
+
+
+
---
+++ Python/Prueba1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 25 13:35:57 2024
+
+@author: Yorvi
+"""
+
